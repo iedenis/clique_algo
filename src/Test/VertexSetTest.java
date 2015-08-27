f781d0f31976c3cf62a03bf25f2bfe6425b31ea3
Fox <denis.ievlev@gmail.com>
2015-08-27 20:00:53 +0300

    All source files moved to the Main package and the testing classes moved to the Test package

---
+++ src/Test/VertexSetTest.java
@@ -0,0 +1,72 @@
+package Test;
+
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+import Main.VertexSet;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class VertexSetTest {
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	@Before
+	public void setUp() throws Exception {
+
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testVertexSet() {
+	}
+
+	@Test
+	public void testContains() {
+	}
+
+	@Test
+	public void testVertexSetVertexSet() {
+	}
+
+	@Test
+	public void testAdd() {
+		
+	}
+
+	@Test
+	public void testSize() {
+		VertexSet vs=new VertexSet();
+		vs.add(1);
+assertTrue(true);
+	}
+
+	@Test
+	public void testAt() {
+	}
+
+	@Test
+	public void testToString() {
+	}
+
+	@Test
+	public void testToFile() {
+	}
+
+	@Test
+	public void testIntersection() {
+	}
+
+}
