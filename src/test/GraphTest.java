40ed29f190223dc7007e9196e52364d930a8ea82
Fox <denis.ievlev@gmail.com>
2015-08-27 04:44:23 +0300

    added new not implemented JUnit test classes to the package "test"

---
+++ src/test/GraphTest.java
@@ -0,0 +1,80 @@
+package test;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class GraphTest {
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testGraph() {
+		//fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetBiggestClique() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testNi() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testPrint() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testPrint1() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testAll_Cliques_DFS_2() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testWrite2file_new() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testAll_Cliques_DFS() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testAllC_seed() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testWrite2file() {
+		fail("Not yet implemented");
+	}
+
+}
